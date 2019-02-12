271f513d38576cede499d1d9a10abe660d9065ff
Vl <Vl@192.168.0.12>
2019-02-12 15:30:19 +0200

    zagruzka3

---
+++ Tasks2/TemplateTest.php
@@ -0,0 +1,7 @@
+<?php header("Content-Type: text/html;charset=utf-8"); ?>
+<meta http-equiv="Content-Type" content="text/html;charset=utf-8"/>
+<?php
+$pageTitle = 'Пример шаблона',
+$array = array('one','two','there');
+require 'Template.php';
+
---
+++ Tasks2/delete.php
@@ -0,0 +1,15 @@
+<?php
+$id = $ GET['id'];
+
+$pdo = new PDO("mysql:host=localhost; dbname=test", "root","");
+
+$sql = 'DELETE FROM tasks WHERE id=:id';
+$statement = $pdo->prepare($sql);
+$statement->bindParam(":id", $id);
+$statemet->execute();
+
+header('Location: /');
+
+
+
+
