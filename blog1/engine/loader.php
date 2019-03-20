32e7ef76ceb249e69544ffaf0cdda8bd80e41b1e
Vl <Vl@192.168.0.12>
2019-03-20 15:42:11 +0200

    555

---
+++ blog1/engine/loader.php
@@ -0,0 +1,28 @@
+<?php
+namespace TestProject\Engine;
+use TestProject\Engine\Pattern\Singleton;
+
+require_once_DIR_ . '/Pattern/Base.trait.php';
+require_once_DIR_. '/Pattern/Singleton.trait.php';
+class Loader
+{
+    use Singleton;
+    public function init()
+    {
+        spl_autoload_register(arry(_CLASS_,'_loadClasses'));
+    }
+    private function _loadClasses($sClass)
+    {
+     $sClass = str_replace(array(_NAMESPASE_,'TestProject', '\\'),'/',$sClass);
+     if (is_file(_DIR_ . '/'.$sClass . '.php')){
+         require_once ROOT_PATH . $sClass . '.php';
+     }
+         
+     }
+     
+        
+    }
+
+
+
+
