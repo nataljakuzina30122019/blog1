b5c351dc856c0b8afce14ece9f6126c61a0b135d
Vl <Vl@192.168.0.12>
2019-03-20 15:58:06 +0200

    555

--- Tasks2/TemplateTest.php
+++
@@ -1,7 +0,0 @@
-<?php header("Content-Type: text/html;charset=utf-8"); ?>
-<meta http-equiv="Content-Type" content="text/html;charset=utf-8"/>
-<?php
-$pageTitle = 'Пример шаблона',
-$array = array('one','two','there');
-require 'Template.php';
-
--- Tasks2/store.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$pdo = new PDO("mysql:host=localhost; dbname=test", "root", "");
-$sql = "INSERT INTO tasks (title,content)VALUESm(:title, :content)";
-$statement = $pdo->prepare($sql);
-$statement->execute($ POST);
-
-
